446e194a41e1ce1ff6e597d402d8b7f4d172ce73
Nikita Peplov <npeplov@yandex.ru>
2020-11-13 16:42:25 +0300

    createRequest: removed xhr.open/send code duplicate from if()

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,9 +1,4 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options, callback) => {
-    // console.log(options);
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     if (options.responseType)
@@ -11,57 +6,39 @@ const createRequest = (options, callback) => {
     xhr.setRequestHeader = options.headers;
 
     xhr.withCredentials = true;
-
-    // if (options.method === 'GET')
-    //     console.log(options);
+    let url, formData;
 
     if (options.method === 'GET' && options.id) {
-        let url = options.url + '/' + options.id
-        xhr.open(options.method, url);
-        xhr.send();
+        url = options.url + '/' + options.id;
     }
 
     else if (options.method === 'GET') {
-        let url = options.url + "?";
+        url = options.url + "?";
         for (let key in options.data)
             url += key + "=" + options.data[key] + "&";
-        
-        xhr.open( options.method, url.slice(0, -1) );
-        xhr.send();
+
+        url = url.slice(0, -1);
     }
 
     else {
-        const formData = new FormData;
+        formData = new FormData;
+        url = options.url;
+
         for (let key in options.data)
             formData.append( key, options.data[key] );
-
-        xhr.open(options.method, options.url);
-        xhr.send(formData);
-        
-        // for(let pair of formData.entries()) { console.log(pair[0]+ ', '+ pair[1]);}
     }
+
+    xhr.open(options.method, url);
+    xhr.send(formData);
 
     xhr.onload = () => {
         if (xhr.response.success) {
-            const err = null;
-            callback(err, xhr.response);
-            // console.log('ok', xhr.response);
+            callback(null, xhr.response);
         }
         else {
-            console.log('ERROR', xhr.response);
             callback(xhr.response);
         }
     }
-
-//  Если в процессе запроса произойдёт ошибка, её объект
-//  должен быть в параметре err.
-//  response success: false; error: "E-Mail адрес 1@1.ru уже существует."
-
     return xhr.response;
-};
-
-// createRequest( {
-//     url: '', method: 'GET', responseType: 'json', callback: (response) => {console.log(response)},
-//     data: { mail: 'ivan@biz.pro', password: 'odinodin'}
-// });
+}
 
